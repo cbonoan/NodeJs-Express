0aeac765bce6711b0dcdff50dd982cf34fa542d7
Charles Bonoan <charlesbonoan@gmail.com>
2021-10-03 19:05:17 -0700

    completed first express app

---
+++ firstapp/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+// app.use((req, res) => {
+//     console.log("Request made!");
+//     res.send("WE GOT YOUR REQUEST!");
+// })
+
+// Routing 
+app.get("/cats", (req, res) =>{
+    res.send("MEOW")
+})
+
+app.get("/dogs", (req, res) =>{
+    res.send("BORK")
+})
+
+app.get("/", (req, res) =>{
+    res.send("HOME!!!")
+})
+
+// Routing with patterns
+app.get("/r/:subreddit", (req, res) =>{
+    console.log(req.params);
+    res.send("THIS IS A SUBREDDIT");
+})
+
+// Routing with queries
+app.get("/search", (req,res) => {
+    console.log(req.query);
+    res.send("hi");
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000!");
+})
