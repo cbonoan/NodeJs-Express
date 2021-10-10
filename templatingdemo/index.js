c702e7772c6c43f3c22e4fd1be67238d994b46f6
Charles Bonoan <charlesbonoan@gmail.com>
2021-10-09 18:00:03 -0700

    learning mongodb and getting started with mongoose

---
+++ templatingdemo/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// setting templating engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get("/", (req,res) => {
+    res.render('home');
+})
+
+app.get("/rand", (req,res) => {
+    const num = Math.floor(Math.random()*10) + 1;
+    res.render('random', {rand: num});
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000");
+})
